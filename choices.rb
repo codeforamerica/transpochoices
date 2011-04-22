af4231c8241aacd9b680f90b6297401be7aa59a9
T S <YenTheFirst@gmail.com>
2011-04-22 15:04:10 -0700

    migrated to bing, calculate transit stuff

--- choices.rb
+++ choices.rb
@@ -1,33 +1,89 @@
 require 'net/http'
 
-get "/info_for_route" do
-	base_url="http://maps.googleapis.com/maps/api/directions/json?origin=#{params[:origin]}&destination=#{params[:destination]}&sensor=false"
-	modes=%w{driving walking bicycling}
+def get_info_from_bing(params)
+	base_url="http://dev.virtualearth.net/REST/v1/Routes/"
+	query_params = "?wayPoint.1=#{params[:origin]}&waypoint.2=#{params[:destination]}&dateTime=#{Time.now.strftime("%H:%M")}&timeType=Arrival&key=#{ENV['BING_KEY']}"
+	modes=%w{driving walking transit}
+	
 	results =modes.map do |mode|
-		response = JSON.parse(Net::HTTP.get(URI.parse(base_url+"&mode=#{mode}")))
-		legs=response["routes"][0]["legs"]
-		info={
-			:distance=>{:unit=>"meters",:value=>legs.inject(0) {|s,x| s+x["distance"]["value"]}},
-			:duration=>{:unit=>"seconds",:value=>legs.inject(0) {|s,x| s+x["duration"]["value"]}}
-		}
-		[mode,info]
+		begin
+			usable_url=URI.parse(URI.escape(base_url+mode+query_params))
+			response = JSON.parse(Net::HTTP.get(usable_url))
+			
+			resource = response["resourceSets"][0]["resources"][0]
+			info = {
+				:distance=>resource["travelDistance"],
+				:duration=>resource["travelDuration"]
+			}
+			[mode,resource]
+		rescue
+			[mode,nil]
+		end
 	end
-	results = Hash[*results.flatten]
+	Hash[*results.flatten]
+end
+
+def generic_by_bing_resource(resource)
+	{
+		:distance=>resource["travelDistance"],
+		:duration=>resource["travelDuration"]
+	}
+end
+
+def calculate_transit_by_bing_resource(resource)
+	info_by_type = resource["routeLegs"].map do |leg|
+		leg["itineraryItems"].map do |item|
+			{
+				:type=>item["details"][0]["maneuverType"],
+				:distance=>item["travelDistance"],
+				:duration=>item["travelDuration"]
+			}
+		end
+	end.flatten.group_by {|i| i[:type]}
 	
-	results["driving"].merge!({
-		:emmisions=>{:unit=>"bunnies killed",:value=>1},
-		:cost=>{:unit=>"USD",:value=>1}
-	})
-	results["walking"].merge!({
-		:emmisions=>{:unit=>"bunnies killed",:value=>3},
-		:cost=>{:unit=>"USD",:value=>2}
-	})
-	results["bicycling"].merge!({
-		:emmisions=>{:unit=>"bunnies killed",:value=>5},
-		:cost=>{:unit=>"USD",:value=>4}
-	})
+	{
+		:duration=>resource["travelDuration"],
+		:while_walking=>{
+			:distance => info_by_type["Walk"].inject(0) {|s,x| s+x[:distance]},
+			:duration => info_by_type["Walk"].inject(0) {|s,x| s+x[:duration]}
+		},
+		:while_transit=>{
+			:duration => info_by_type["TakeTransit"].inject(0) {|s,x| s+x[:duration]}
+		}
+	}
+end
+
+get "/info_for_route_bing" do
+	results = get_info_from_bing(params)
 	
-	[200,{},results.to_json]
+	if params[:raw_data]=="yes_please"
+		puts "here"
+		return [200,{},JSON.pretty_generate(results)] 
+	end
+	
+	if (resource=results["driving"])
+		results["driving"]=generic_by_bing_resource(resource)
+	end
+	if (resource=results["walking"])
+		results["walking"]=generic_by_bing_resource(resource)
+		results["biking"]=generic_by_bing_resource(resource)
+		results["biking"]["travelDuration"] /= 4.0
+	end
+	if (resource=results["transit"])
+		results["transit"] = calculate_transit_by_bing_resource(resource)
+	end
+	
+	output = {:units=>
+		{
+			:distance=>"kilometers",
+			:duration=>"seconds",
+			:emissions=>"milliliters of innocent bunny blood",
+			:cost=>"USD",
+			:calories=>"calories"
+		},
+		:results=>results
+	}
+	[200,{},JSON.pretty_generate(output)]
 end
 
 get "/" do
