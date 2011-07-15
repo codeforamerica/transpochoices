c0efe27ed62f7cbc61da7241b2f3214520b33964
T S <YenTheFirst@gmail.com>
2011-07-15 14:21:28 -0700

    get Rack::Test working to test requests against the app

--- spec/choices_spec.rb
+++ spec/choices_spec.rb
@@ -1,9 +1,11 @@
 require 'helper'
 
 describe "get info" do
+
 	before :each do
 		ENV['BING_KEY'] = "test_key"
 		
+		@boston_params = {:origin => "1 State St, Boston, MA 02109, USA", :destination => "1 Kneeland St, Boston, MA 02111, USA", :time=>"11:51"}
 		@base_url = "http://dev.virtualearth.net/REST/v1/Routes/"
 		@boston_bing_query = "?wayPoint.1=1%20State%20St,%20Boston,%20MA%2002109,%20USA&waypoint.2=1%20Kneeland%20St,%20Boston,%20MA%2002111,%20USA&dateTime=11:51&timeType=Arrival&key=test_key"
 		#stub the boston requests
@@ -14,13 +16,15 @@ describe "get info" do
 	end
 	
 	it "should make bing requests for each of the transit modes" do
-		get_info_from_bing({:origin => "1 State St, Boston, MA 02109, USA", :destination => "1 Kneeland St, Boston, MA 02111, USA", :time=>"11:51"})
+		get_info_from_bing(@boston_params)
 		%w(transit driving walking).each do |mode|
 			a_request(:get,"#{@base_url}#{mode}#{@boston_bing_query}").should have_been_made
 		end
 	end
 	
-	pending "should calculate fare for transit" do
+	it "should calculate fare for transit" do
+		get "/info_for_route_bing", @boston_params
+		JSON.parse(last_response.body)["results"]["transit"]["cost"].should == 3 #or whatever number.
 	end
 	
 	pending "should return null fare when transit GTFS data is unavailable" do
