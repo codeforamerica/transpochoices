9f8dbc79b915aa0eab37992c39e022c2f02020a6
T S <YenTheFirst@gmail.com>
2011-07-15 11:43:37 -0700

    describe initial specs

---
+++ spec/choices_spec.rb
@@ -0,0 +1,10 @@
+describe "get info" do
+	pending "should make bing requests for each of the transit modes" do
+	end
+	
+	pending "should calculate fare for transit" do
+	end
+	
+	pending "should return null fare when transit GTFS data is unavailable" do
+	end
+end
