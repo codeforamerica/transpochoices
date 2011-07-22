cc4b843a5273265cf03ff421a64a6ba2b30b0545
T S <YenTheFirst@gmail.com>
2011-07-22 13:44:00 -0700

    added first failing seattle case

--- spec/choices_spec.rb
+++ spec/choices_spec.rb
@@ -34,4 +34,23 @@ describe "get info" do
 		get "/info_for_route_bing", @boston_params
 		JSON.parse(last_response.body)["results"]["transit"]["cost"].should be_nil
 	end
+	
+	describe "seattle multi-city" do
+		before :each do
+			@seattle_bing_query = "?wayPoint.1=Seattle,%20WA,%20USA&waypoint.2=Everett,%20WA,%20USA&dateTime=13:33&timeType=Arrival&key=test_key"
+			@seattle_params = {:origin=>"Seattle, WA, USA",:destination=>"Everett, WA, USA", :time=>"13:33"}
+			
+			%w(driving walking).each do |mode|
+				stub_request(:get,"#{@base_url}#{mode}#{@boston_bing_query}").
+				to_return(:body=>"[]")
+			end
+			stub_request(:get,"#{@base_url}transit#{@boston_bing_query}").
+				to_return(:body=>fixture("failing_seattle.json"))
+		end
+		
+		it "shouldn't fail" do
+			get "/info_for_route_bing", @seattle_params
+			JSON.parse(last_response.body)["results"]["transit"].should_not be_nil
+		end
+	end
 end
